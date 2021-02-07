3981d8c0cd4dadb6f6221e2d4e1973e6007454cc
trantrieudang <trieudang.tran@gmail.com>
2021-02-07 21:30:05 +0700

    testtttt

--- src/containers/HomeTemplate/HomePage/components/Footer/index.js
+++ src/containers/HomeTemplate/HomePage/components/Footer/index.js
@@ -1,4 +1,70 @@
 import React from "react";
+import cgv from "../../../../../assets/images/cgv.png";
+import bhd from "../../../../../assets/images/bhd.png";
+import glx from "../../../../../assets/images/galaxycine.png";
+import star from "../../../../../assets/images/cinestar.png";
+import lotte from "../../../../../assets/images/lotte.png";
+import mega from "../../../../../assets/images/megags.png";
+import beta from "../../../../../assets/images/bt.jpg";
+import ddc from "../../../../../assets/images/dongdacinema.png";
+import touch from "../../../../../assets/images/TOUCH.png";
+import cinemax from "../../../../../assets/images/cnx.jpg";
+import starlight from "../../../../../assets/images/STARLIGHT.png";
+import cine from "../../../../../assets/images/dcine.png";
+import zalo from "../../../../../assets/images/zalopay_icon.png";
+import payoo from "../../../../../assets/images/payoo.jpg";
+import vcb from "../../../../../assets/images/VCB.png";
+import agri from "../../../../../assets/images/AGRIBANK.png";
+import vietin from "../../../../../assets/images/VIETTINBANK.png";
+import ivb from "../../../../../assets/images/IVB.png";
+import go from "../../../../../assets/images/123go.png";
+import laban from "../../../../../assets/images/laban.png";
+import appleLogo from "../../../../../assets/images/apple-logo.png";
+import androidLogo from "../../../../../assets/images/android-logo.png";
+import facebookLogo from "../../../../../assets/images/facebook-logo.png";
+import zaloLogo from "../../../../../assets/images/zalo-logo.png";
+import zionLogo from "../../../../../assets/images/zion-logo.jpg";
+import boCongThuong from "../../../../../assets/images/bocongthuong.png";
+
+import { Link } from "react-router-dom";
+
+const listBrands1 = [
+  { id: "1", logo: `${cgv}` },
+  { id: "2", logo: `${bhd}` },
+  { id: "3", logo: `${glx}` },
+  { id: "4", logo: `${star}` },
+  { id: "5", logo: `${lotte}` },
+  
+];
+
+const listBrands2 = [
+  { id: "1", logo: `${mega}` },
+  { id: "2", logo: `${beta}` },
+  { id: "3", logo: `${ddc}` },
+  { id: "4", logo: `${touch}` },
+  { id: "5", logo: `${cinemax}` },
+  
+];
+
+const listBrands3 = [
+  { id: "1", logo: `${starlight}` },
+  { id: "2", logo: `${cine}` },
+  { id: "3", logo: `${zalo}` },
+  { id: "4", logo: `${payoo}` },
+  { id: "5", logo: `${vcb}` },
+  
+];
+
+const listBrands4 = [
+  { id: "1", logo: `${agri}` },
+  { id: "2", logo: `${vietin}` },
+  { id: "3", logo: `${ivb}` },
+  { id: "4", logo: `${go}` },
+  { id: "5", logo: `${laban}` },
+  
+];
+
+
 
 export default function Footer() {
   return (
@@ -26,108 +92,81 @@ export default function Footer() {
             <div className="col-sm-4 col-xs-12 doiTac hideOnMobile">
               <p>Đối Tác</p>
               <div className="row">
-                <a href>
-                  <img className="icon" src="./img/cgv.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/bhd.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/galaxycine.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/cinestar.png" alt />
-                </a>
-                <a href>
-                  <img
-                    className="icon"
-                    src="./img/404b8c4b80d77732e7426cdb7e24be20.png"
-                    alt
-                  />
-                </a>
+                
+                {listBrands1.map((item) => {
+                  console.log(item);
+                  return (
+                    
+                    
+                    <Link to="/" key={item.id}>
+                      <img className="icon" src={item.logo} alt={item.logo} />
+                    </Link>
+                    
+                  );
+                })}
               </div>
               <div className="row">
-                <a href>
-                  <img className="icon" src="./img/cgv.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/bhd.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/galaxycine.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/cinestar.png" alt />
-                </a>
-                <a href>
-                  <img
-                    className="icon"
-                    src="./img/404b8c4b80d77732e7426cdb7e24be20.png"
-                    alt
-                  />
-                </a>
+                
+                {listBrands2.map((item) => {
+                  console.log(item);
+                  return (
+                    
+                    
+                    <Link to="/" key={item.id}>
+                      <img className="icon" src={item.logo} alt={item.logo} />
+                    </Link>
+                    
+                  );
+                })}
               </div>
               <div className="row">
-                <a href>
-                  <img className="icon" src="./img/cgv.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/bhd.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/galaxycine.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/cinestar.png" alt />
-                </a>
-                <a href>
-                  <img
-                    className="icon"
-                    src="./img/404b8c4b80d77732e7426cdb7e24be20.png"
-                    alt
-                  />
-                </a>
+                
+                {listBrands3.map((item) => {
+                  console.log(item);
+                  return (
+                    
+                   
+                    <Link to="/" key={item.id}>
+                      <img className="icon" src={item.logo} alt={item.logo} />
+                    </Link>
+                    
+                  );
+                })}
               </div>
               <div className="row">
-                <a href>
-                  <img className="icon" src="./img/cgv.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/bhd.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/galaxycine.png" alt />
-                </a>
-                <a href>
-                  <img className="icon" src="./img/cinestar.png" alt />
-                </a>
-                <a href>
-                  <img
-                    className="icon"
-                    src="./img/404b8c4b80d77732e7426cdb7e24be20.png"
-                    alt
-                  />
-                </a>
+                
+                {listBrands4.map((item) => {
+                  console.log(item);
+                  return (
+                    
+                    
+                    <Link to="/" key={item.id}>
+                      <img className="icon" src={item.logo} alt={item.logo} />
+                    </Link>
+                    
+                  );
+                })}
               </div>
+              
             </div>
             <div className="col-sm-4 col-xs-12 mediaApp">
               <div className="row">
                 <div className="col-xs-6 mobileApp hideOnMobile">
                   <p>MOBILE APP</p>
                   <a className="col-xs-6" href>
-                    <img src="./img/apple-logo.png" alt />
+                    <img src={appleLogo} alt={appleLogo} />
                   </a>
                   <a className="col-xs-6" href>
-                    <img src="./img/android-logo.png" alt />
+                    <img src={androidLogo} alt={androidLogo} />
                   </a>
                 </div>
                 <div className="col-xs-6 social m-auto text-center">
                   <p className="hideOnMobile">Social</p>
                   <a className="col-xs-6" href>
-                    <img src="./img/facebook-logo.png" alt />
+                    <img src={facebookLogo} alt={facebookLogo} />
                   </a>
                   <a className="col-xs-6" href>
-                    <img src="./img/zalo-logo.png" alt />
+                    <img src={zaloLogo} alt={zaloLogo} />
                   </a>
                 </div>
               </div>
@@ -136,7 +175,7 @@ export default function Footer() {
           <hr />
           <div className="row bottomContain">
             <div className="col-sm-1 col-xs-12 zion">
-              <img className="d-block m-auto" src="./img/zion-logo.jpg" alt />
+              <img className="d-block m-auto" src={zionLogo} alt={zionLogo} />
             </div>
             <div className="col-sm-9 col-xs-12 d-block ">
               <span className="tix_info">
@@ -166,8 +205,8 @@ export default function Footer() {
             <div className="col-sm-1 col-xs-12 bct">
               <img
                 className="d-block m-auto "
-                src="./img/d1e6bd560daa9e20131ea8a0f62e87f8.png"
-                alt
+                src={boCongThuong}
+                alt={boCongThuong}
               />
             </div>
           </div>
