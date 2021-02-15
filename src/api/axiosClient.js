51c5b26876f8e3b7e5a26b5528dfd0bdf7ae1e8f
trantrieudang <trieudang.tran@gmail.com>
2021-02-15 12:05:18 +0700

    testsss

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,27 +1,48 @@
 import axios from 'axios';
-import queryString from 'query-string';
 
 const axiosClient = axios.create({
      baseURL: 'https://movie0706.cybersoft.edu.vn/api',
      headers: {
           'content-type': 'application/json',
      },
-     paramsSerializer: params => queryString.stringify(params),
+    
 });
 
-axiosClient.interceptors.request.use(async (config) => {
-     //handle token here...
-     return config;
-});
-
-axiosClient.interceptors.response.use((response) => {
-     if(response && response.data){
-          return response.data;
+// Interceptors
+// Add a request interceptor
+axiosClient.interceptors.request.use(
+     function (config) {
+       // Do something before request is sent
+       return config;
+     },
+     function (error) {
+       // Do something with request error
+       return Promise.reject(error);
      }
-     return response;
-}, (error) => {
-     //handle error here...
-     throw error;
-});
+   );
+   
+   // Add a response interceptor
+   axiosClient.interceptors.response.use(
+     function (response) {
+       // Any status code that lie within the range of 2xx cause this function to trigger
+       // Do something with response data
+       return response.data;
+     },
+     function (error) {
+       // Any status codes that falls outside the range of 2xx cause this function to trigger
+       // Do something with response error
+       const { config, status, data } = error.response;
+       const URLS = ['/QuanLyNguoiDung/DangKy', '/QuanLyNguoiDung/DangNhap'];
+       if (URLS.includes(config.url) && status === 400) {
+         const errorList = data.data || [];
+         const firstError = errorList.length > 0 ? errorList[0] : {};
+         const messageList = firstError.messages || [];
+         const firstMessage = messageList.length > 0 ? messageList[0] : {};
+         throw new Error(firstMessage.message);
+       }
+   
+       return Promise.reject(error);
+     }
+   );
 
 export default axiosClient;
