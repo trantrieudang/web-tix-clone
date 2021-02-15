3a1e2905d6a4eb5437426f9a756d9d9d41d8c3ed
trantrieudang <trieudang.tran@gmail.com>
2021-02-15 11:41:34 +0700

    test

--- src/containers/AdminTemplate/AuthPage/components/userSlice.js
+++ src/containers/AdminTemplate/AuthPage/components/userSlice.js
@@ -2,15 +2,15 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import userApi from '../../../../api/userApi';
 import StorageKeys from '../../../../constants/storage-keys';
 
-// export const register = createAsyncThunk('user/register', async (payload) => {
-//   const data = await userApi.register(payload);
+export const register = createAsyncThunk('user/register', async (payload) => {
+  const data = await userApi.register(payload);
 
-//   // save data to local storage
-//   localStorage.setItem(StorageKeys.TOKEN, data.jwt);
-//   localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
+  // save data to local storage
+  localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
 
-//   return data.user;
-// });
+  return data.user;
+});
 
 export const login = createAsyncThunk('user/login', async (payload) => {
   const data = await userApi.login(payload);
@@ -38,9 +38,9 @@ const userSlice = createSlice({
     },
   },
   extraReducers: {
-    // [register.fulfilled]: (state, action) => {
-    //   state.current = action.payload;
-    // },
+    [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
 
     [login.fulfilled]: (state, action) => {
       state.current = action.payload;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
-import HomePage from "./../containers/HomeTemplate/HomePage";
-import Login from "./../containers/AdminTemplate/AuthPage/components/Login";
+import HomePage from "../containers/HomeTemplate/HomePage";
+import Login from "../containers/AdminTemplate/AuthPage/components/Login";
 
 import DashboardPage from "../containers/AdminTemplate/DashboardPage";
 
